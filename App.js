5d35ba3ca012e806ee686c57ee9775f53f0adad3
IidaPeltonen <n0peii00@students.oamk.fi>
2022-09-21 14:40:58 +0300

    flatti ei toimi

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import DATA from './Data';
 
 export default function App() {
+/* 
+  let testData = []
+  for (let i = 0 ; i < 50; i++) {
+    testData = [...DATA, ...testData]
+  } */
+
+  const Item = ({person}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{marginTop: StatusBar.currentHeigth || 20}}>
+          {/* <ScrollView> 
+            {testData.map((item, index) => 
+            <Text style={{fontSize:20}} key={index}>{item.fname}</Text>)} 
+           </ScrollView>  */}
+    <View style={style.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <Item person={item}/>}
+      />
+    </View> 
     </View>
   );
-}
+          }
+        }
 
-const styles = StyleSheet.create({
+
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: 300,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  itemView: {
+    backgroundColor: '#c8c8c8',
+    padding: 10,
+    margin: 5
+  },
+  itemText: {
+    fontSize: 32,
+    margin: 10
+  }
+})
+  
