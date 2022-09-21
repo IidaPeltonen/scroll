989b015a019fd655e11d453c51693ec1f574ac70
IidaPeltonen <n0peii00@students.oamk.fi>
2022-09-21 19:18:09 +0300

    valmis

--- App.js
+++ App.js
@@ -1,38 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
-import DATA from './Data';
+/* import { StatusBar } from 'expo-status-bar'
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native'
+import DATA from './Data'
 
-export default function App() {
-/* 
+export default function App () {
   let testData = []
-  for (let i = 0 ; i < 50; i++) {
+  for (let i = 0; i < 50; i++) {
     testData = [...DATA, ...testData]
-  } */
+  }
 
-  const Item = ({person}) => {
   return (
-    <View style={{marginTop: StatusBar.currentHeigth || 20}}>
-          {/* <ScrollView> 
-            {testData.map((item, index) => 
-            <Text style={{fontSize:20}} key={index}>{item.fname}</Text>)} 
-           </ScrollView>  */}
-    <View style={style.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item person={item}/>}
-      />
-    </View> 
+    <View style={{ marginTop: StatusBar.currentHeigth || 0 + 20 }}>
+      <ScrollView>
+        {testData.map((item, index) => (
+          <Text style={{ fontSize: 20 }} key={index}>
+            {item.fname}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
-  );
-          }
-        }
-
+  )
+}
 
 const style = StyleSheet.create({
   container: {
     height: 300,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   itemView: {
     backgroundColor: '#c8c8c8',
@@ -43,5 +36,55 @@ const style = StyleSheet.create({
     fontSize: 32,
     margin: 10
   }
+}) */
+
+import { StatusBar } from 'expo-status-bar'
+import { FlatList, ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+import DATA from './Data'
+
+export default function App () {
+
+  return (
+    <ScrollView style={styles.container}>
+    <View>
+      <FlatList
+        data={DATA}
+        renderItem={({item})=> <Item person={item}></Item>}
+      />
+    </View>
+    </ScrollView>
+  )
+}
+
+const Item = ({person}) => {
+  return(
+    <View>
+      <View>
+        <Text style={styles.itemText}>{person.lname} {person.fname}</Text>
+        <Image style={styles.image} source={{uri: person.img}} />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  itemView: {
+    backgroundColor: '#c8c8c8',
+    padding: 10,
+    margin: 5
+  },
+  itemText: {
+    fontSize: 32,
+    margin: 10
+  },
+  image: {
+    width: 100,
+    height: 150
+  }
 })
-  
+
+
